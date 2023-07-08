90025e6123dde3b16de6b5300d0b415acad08e12
jonathan sand <sand@gizmolab.com>
2023-07-08 12:44:42 -0700

    use auto-subscribe

--- src/lib/seriously/components/Text.svelte
+++ src/lib/seriously/components/Text.svelte
@@ -1,43 +1,34 @@
 <svelte:options immutable = {true} />
 
 <script lang='ts'>
-  import { Entity, entities, editingID, onMount, onDestroy, signal, SignalKinds } from '../common/imports.ts';
+  import { Entity, entities, editingID, grabbing, signal, SignalKinds } from '../common/imports.ts';
   import Widget from './Widget.svelte';
 	export let entity = Entity;
-  let unsubscribe;
   let input;
   
-  function subscribe() {
-    unsubscribe = editingID.subscribe((editing) => {
-      setTimeout(() => {     // need to wait for the input element to be fully instantiated
-        if ($editingID == entity.id) {
-          input.focus();
-          // console.log('AUTO EDIT:', entity.lineAttribute, entity.title);
-        } else {
-          input.blur();
-          entities.updateToCloud(entity);
-          // console.log('AUTO     :', entity.lineAttribute, entity.title);
-        }
-        // console.log('FOCUS:', entity.borderAttribute, entity.title);
-        signal([SignalKinds.widget], null); // so widget will show as [un]grabbed
-      }, 50);    // wait long enough to let editingID to update before reading it
-    });
+  $: {
+    setTimeout(() => {     // wait until the input element is fully instantiated and editingID is settled
+      if ($editingID == entity.id) {
+        input.focus();
+      } else {
+        input.blur();
+        entities.updateToCloud(entity); // TODO: PERFORMANCE only update if dirty
+      };
+      signal([SignalKinds.widget], null); // so widget will show as [un]grabbed
+    }, 50);
   }
 
   function handleFocus(event) {
     $editingID = entity.id;
-    entity.grabOnly();
+    grabbing.grabOnly(entity)
     signal([SignalKinds.widget], null); // so widget will show as grabbed
-    // console.log('EDIT:', entity.title, entity.lineAttribute);
   }
 
   function handleKeyDown(event) {
     if ($editingID == entity.id) {
-      // const COMMAND = event.metaKey;
       if (event.key == 'Enter') {
         input.blur();
         entities.updateToCloud(entity);
-        // console.log('STOP:', entity.title);
         setTimeout(() => { // need to wait so subscribe still matches the editingID
           $editingID = undefined;
         }, 20);
@@ -47,9 +38,6 @@
 
   function handleInput(event) { entity.title = event.target.value; }
   function handleBlur(event) { $editingID = undefined; }
-  
-  onMount(subscribe);
-  onDestroy(unsubscribe);
 </script>
 
 <input
--- src/lib/seriously/data/Entity.ts
+++ src/lib/seriously/data/Entity.ts
@@ -1,7 +1,6 @@
 import Airtable from '../../../../node_modules/airtable/lib/airtable';
 import { grabbing, editingID } from '../common/imports';
 import { seriouslyGlobals } from './Globals';
-import { derived } from 'svelte/store';
 import { v4 as uuid } from 'uuid';
 
 export default class Entity {
@@ -10,6 +9,7 @@ export default class Entity {
   color: string;
   trait: string;
   parent: Entity | null;
+  isEditing: boolean;
 
   constructor(id = uuid().string, title = 'Entity', color = 'black', trait = 's') {
     this.id = id;
@@ -17,17 +17,19 @@ export default class Entity {
     this.color = color;
     this.trait = trait;
     this.parent = null;
+    this.isEditing = false;
+
+    editingID.subscribe((id: string) => {
+      this.isEditing = id == this.id; // executes whenever editingID changes
+    });
   };
 
   get fields(): Airtable.FieldSet { return { title: this.title, color: this.color, trait: this.trait }; }
   get  grabAttributes(): string { return this.borderAttribute + this.hoverColor(false); }
   get hoverAttributes(): string { return this.borderAttribute + this.hoverColor(true); }
-  get borderAttribute(): string { return editingID === this.id ? 'dashed' : 'solid' + ' 1px '; }
-  get isGrabbed(): boolean { return grabbing.isGrabbed(this); }
-  get isEditing(): boolean { return editingID === this.id }
-  grabOnly() { grabbing.grabOnly(this); }
+  get borderAttribute(): string { return this.isEditing ? 'dashed' : 'solid' + ' 1px '; }
   
   hoverColor(hovering: boolean): string {
-    return (this.isGrabbed != hovering) ? this.color : seriouslyGlobals.backgroundColor;
+    return (grabbing.isGrabbed(this) != hovering) ? this.color : seriouslyGlobals.backgroundColor;
   }
 }
