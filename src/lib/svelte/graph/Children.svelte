620043a946671a4de4ef7dddee340e62baa0f790
jonathan sand <sand@gizmolab.com>
2023-09-25 13:17:44 -0700

    denter drag dots at end of lines

--- src/lib/svelte/graph/Children.svelte
+++ src/lib/svelte/graph/Children.svelte
@@ -1,19 +1,20 @@
 <script lang=ts>
 	import { Rect, Size, Point, Thing, Signals, Layout, onMount, onDestroy, LineRect, LineCurveType, normalizeOrderOf, handleSignalOfKind } from '../../ts/common/GlobalImports';
-	import { widgetHeightGap } from '../../ts/managers/State';
+	import { lineGap } from '../../ts/managers/State';
 	import Widget from './Widget.svelte';
 	import Line from './Line.svelte';
 	export let lineRects: Array<LineRect> = [];
+	const offset = new Point(10, -14);   // TODO: WHY is this needed, where does this value come from?
 	export let thing: Thing;
 
 	let toggleDraw = false;
 	let children = thing.children;
 	onDestroy( () => {signalHandler.disconnect(); });
 	function lineRectAt(index: number): LineRect { return lineRects[index]; }
-	function lineTypeAt(index: number): number { return lineRectAt(index).lineType; }
+	function curveTypeAt(index: number): number { return lineRectAt(index).curveType; }
 
 	function drawnSize(): Size {
-		const height = $widgetHeightGap;
+		const height = $lineGap;
 		return new Size(100, children.length * height);
 	}
 
@@ -33,8 +34,8 @@
 {#key toggleDraw}
 	{#if children && children.length != 0 && lineRects.length == children.length}
 		{#each children as child, index}
-			<Widget thing={child} origin={lineRectAt(index).extent.offsetBy(new Point(18, -14))}/>
-			<Line color={child.color} curveType={lineTypeAt(index)} rect={lineRectAt(index)}/>
+			<Widget thing={child} origin={lineRectAt(index).extent.offsetBy(offset)}/>
+			<Line color={child.color} curveType={curveTypeAt(index)} rect={lineRectAt(index)}/>
 		{/each}
 	{/if}
 {/key}
