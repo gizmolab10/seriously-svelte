baacc22d941db5e697896f57fd456da7ac45dba8
jonathan sand <sand@gizmolab.com>
2023-10-19 23:20:17 -0700

    click on title ⇨ shifts it one pixel right & down

--- src/lib/svelte/graph/Widget.svelte
+++ src/lib/svelte/graph/Widget.svelte
@@ -17,7 +17,7 @@
 	function updateBorderStyle() {
 		const showBorder = isEditing || isGrabbed;
 		thing.updateColorAttributes();
-		delta = showBorder ? 0 : 1;
+		delta = showBorder ? -1 : 0;
 		border = showBorder ? 'border: ' + thing.grabAttributes : '';
 		background = showBorder ? 'background-color: ' + k.backgroundColor : '';
 	}
@@ -41,7 +41,7 @@
 	<div
 		bind:this={widget}
 		style='z-index: {ZIndex.highlights};
-			top: {origin.y + delta}px;
+			top: {origin.y + delta + 1}px;
 			left: {origin.x + delta}px;
 			padding: {thing.isExemplar ? 1 : 0}px 12px {thing.isExemplar ? 0 : 1}px 1px;
 			{background};
