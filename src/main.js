a68eca6becda015c1d5f7fbe79ac800ad5ce20dc
jonathan sand <sand@gizmolab.com>
2023-11-28 16:47:09 -0800

    trial 6

--- src/main.js
+++ src/main.js
@@ -5,10 +5,4 @@ const app = new SeriouslyApp({
 	target: document.getElementById('app')
 })
 
-// NOTE: `ALLOW-FROM` is not supported in most browsers.
-app.use((req, res, next) => {
-	res.setHeader("X-Frame-Options", "ALLOW-FROM https://www.catalist.network");
-	next();
-});
-
 export default SeriouslyApp;
