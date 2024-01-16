7914f11460751fd35a0c564a53d433c26f4f2044
jonathan sand <sand@gizmolab.com>
2024-01-16 10:46:09 -0800

    fix crash after create new child

--- src/lib/svelte/panel/Crumbs.svelte
+++ src/lib/svelte/panel/Crumbs.svelte
@@ -48,8 +48,8 @@
 					left: {size / ((count > 1) ? 3 : 3.3)}px;'>
 					<FatTriangle
 						extra={(count < 2) ? null : svgPath.circle(size, size / 2, new Point(size / -7, size / 4))}
-						strokeColor={grabbedPath.thing(1).color}
-						fillColor={grabbedPath.thing(1).color}
+						strokeColor={grabbedPath.thing(1)?.color}
+						fillColor={grabbedPath.thing(1)?.color}
 						size={size * ((count < 2) ? 1 : 1.5)}
 						position='absolute'
 					/>
