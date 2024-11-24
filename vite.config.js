a5724d3eb292c3cc38084ef6fef643242888e734
jonathan sand <sand@gizmolab.com>
2024-11-24 10:49:24 -0800

    fixed localhost server

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
+	server: {
+		host: true,  // You can also use '0.0.0.0' to bind to all interfaces
+		port: 5173,
+	},
 });
