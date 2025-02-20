8e1e27de4fa606f8a635342bb775d4fffd525a99
jonathan sand <sand@gizmolab.com>
2025-02-20 14:58:55 -0800

    rewrite S_UX to use a closure to assure object exists

--- src/lib/ts/state/S_UX.ts
+++ src/lib/ts/state/S_UX.ts
@@ -7,11 +7,11 @@ export default class S_UX {
 
 	s_thing_pages_byThingID: {[id: string]: S_Thing_Pages} = {};
 	mouse_timer_byName: { [name: string]: Mouse_Timer } = {};
+	s_widget_byAncestryID: { [id: string]: S_Widget } = {};
 	s_rotation_byName: { [name: string]: S_Rotation } = {};
 	g_segment_byName: { [name: string]: G_Segment } = {};
 	s_element_byName: { [name: string]: S_Element } = {};
 	s_mouse_byName: { [name: string]: S_Mouse } = {};
-	s_widget_byAncestryID: { [id: string]: S_Widget } = {};
 
 	//////////////////////////////////////
 	//									//
@@ -34,69 +34,31 @@ export default class S_UX {
 	get isAny_paging_arc_active(): boolean { return this.rotation_states.filter(s => s.isActive).length > 0; }
 	set_g_segment_forName(g_segment: G_Segment, name: string) { return this.g_segment_byName[name] = g_segment; }
 	get isAny_paging_arc_hovering(): boolean { return this.rotation_states.filter(s => s.isHovering).length > 0; }
+	s_mouse_forName(name: string): S_Mouse { return this.assure_forKey_inDict(name, this.s_mouse_byName, () => S_Mouse.empty()); }
+	s_rotation_forName(name: string): S_Rotation { return this.assure_forKey_inDict(name, this.s_rotation_byName, () => new S_Rotation()); }
+	name_from(identifiable: Identifiable, type: T_Element, subtype: string): string { return `${type}(${subtype}) (id '${identifiable.id}')`; }
+	mouse_timer_forName(name: string): Mouse_Timer { return this.assure_forKey_inDict(name, this.mouse_timer_byName, () => new Mouse_Timer()); }
+	s_widget_forAncestry(ancestry: Ancestry): S_Widget { return this.assure_forKey_inDict(ancestry.id, this.s_widget_byAncestryID, () => new S_Widget(ancestry)); }
 
-	name_from(identifiable: Identifiable, type: T_Element, subtype: string): string {
-		return `${type}(${subtype}) (id '${identifiable.id}')`;
-	}
-
-	s_mouse_forName(name: string): S_Mouse {
-		let state = this.s_mouse_byName[name];
-		if (!state) {		// always assure it exists
-			state = S_Mouse.empty();
-			this.s_mouse_byName[name] = state;
-		}
-		return state;
-	}
-
-	mouse_timer_forName(name: string): Mouse_Timer {
-		let timer = this.mouse_timer_byName[name];
-		if (!timer) {		// always assure it exists
-			timer = new Mouse_Timer();
-			this.mouse_timer_byName[name] = timer;
-		}
-		return timer;
-	}
-
-	s_rotation_forName(name: string): S_Rotation {
-		let s_rotation = this.s_rotation_byName[name];
-		if (!s_rotation) {		// always assure it exists
-			s_rotation = new S_Rotation();
-			this.s_rotation_byName[name] = s_rotation;
-		}
-		return s_rotation;
-	}
-
-	s_widget_forAncestry(ancestry: Ancestry): S_Widget {
-		const id = ancestry.id;
-		let s_widget = this.s_widget_forID(id);
-		if (!s_widget) {			// always assure it exists
-			s_widget = new S_Widget(ancestry);
-			this.s_widget_byAncestryID[id] = s_widget;
-		}
-		return s_widget;
+	s_thing_pages_forThingID(id: string | null | undefined): S_Thing_Pages | null {
+		return !id ? null : this.assure_forKey_inDict(id, this.s_thing_pages_byThingID, () => new S_Thing_Pages(id));
 	}
 
 	s_element_for(identifiable: Identifiable | null, type: T_Element, subtype: string): S_Element {
 		const realIdentifiable = identifiable ?? new Identifiable()
 		const name = this.name_from(realIdentifiable, type, subtype);
-		let s_element = this.s_element_forName(name);
-		if (!s_element) {			// always assure it exists
-			s_element = new S_Element(realIdentifiable, type, subtype);
-			this.s_element_byName[name] = s_element;
-		}
-		return s_element;
+		return this.assure_forKey_inDict(name, this.s_element_byName, () => new S_Element(realIdentifiable, type, subtype));
 	}
 
-	s_thing_pages_forThingID(id: string | null | undefined): S_Thing_Pages | null {
-		if (!id) {
-			return null;
+	assure_forKey_inDict<T>(key: string, dict: Dictionary, closure: () => T): T {
+		let result = dict[key];
+		if (!result) {
+			result = closure();
+			if (!!result) {
+				dict[key] = result;
+			}
 		}
-		let s_thing_pages = this.s_thing_pages_byThingID[id];
-		if (!s_thing_pages) {		// always assure it exists
-			s_thing_pages = new S_Thing_Pages(id);
-			this.s_thing_pages_byThingID[id] = s_thing_pages;
-		}
-		return s_thing_pages;
+		return result;
 	}
 
 	createAll_thing_pages_fromDict(dict: Dictionary | null) {
