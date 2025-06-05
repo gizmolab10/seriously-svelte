f40279f32309259b829f0e14e577352d5fb074e8
jonathan sand <sand@gizmolab.com>
2025-06-05 01:34:41 -0700

    use new box around breadcrumbs

--- src/lib/svelte/kit/Box.svelte
+++ src/lib/svelte/kit/Box.svelte
@@ -1,8 +1,8 @@
 <script lang='ts'>
 	import { k, Point, colors } from '../../ts/common/Global_Imports';
 	import Separator from './Separator.svelte';
-    export let separatorThickness: number = k.thickness.separator.thick;
     export let corner_radius: number = k.radius.gull_wings.thick;
+    export let thickness: number = k.thickness.separator.thick;
     export let color: string = colors.separator;
     export let showBottom: boolean = true;
     export let showRight: boolean = true;
@@ -17,102 +17,65 @@
 
 <div class='box {name}'
     style='
-        position: absolute;
         top: {top}px;
+        display: flex;
         left: {left}px;
         width: {width}px;
-        height: {height}px;'>
+        height: {height}px;
+        position: absolute;
+        box-sizing: border-box;
+        flex-direction: column;'>
+    <div style='
+        flex: 1;
+        display: flex;'>
+        {#if showLeft}
+            <Separator 
+                class='left'
+                color={color}
+                length={height + 2}
+                isHorizontal={false}
+                thickness={thickness}
+                origin={new Point(2, 2)}
+				has_thin_divider={false}
+                corner_radius={corner_radius}/>
+        {/if}        
+        <div style='flex: 1;'>
+            <slot />
+        </div>
+        {#if showRight}
+            <Separator
+                color={color}
+                class='right'
+                length={height + 2}
+                isHorizontal={false}
+                thickness={thickness}
+				has_thin_divider={false}
+                origin={new Point(width - 2, 2)}
+                corner_radius={corner_radius}/>
+        {/if}
+    </div>
     {#if showTop}
         <Separator 
             class='top'
             color={color}
             length={width}
 			add_wings={false}
-            horizontal={true} 
+            isHorizontal={true} 
             origin={Point.zero}
+            thickness={thickness}
 			has_thin_divider={false}
-            thickness={separatorThickness}
-            corner_radius={corner_radius}
-        />
+            corner_radius={corner_radius}/>
     {/if}
-    
-    <div class='content-wrapper'>
-        {#if showLeft}
-            <Separator 
-                class='left'
-                color={color}
-                length={height}
-                horizontal={false} 
-                origin={Point.zero}
-				has_thin_divider={false}
-                corner_radius={corner_radius}
-                thickness={separatorThickness}
-            />
-        {/if}
-        
-        <div class='content'>
-            <slot />
-        </div>
-        
-        {#if showRight}
-            <Separator
-                color={color}
-                class='right'
-                length={height}
-                horizontal={false} 
-                origin={Point.x(width)}
-				has_thin_divider={false}
-                corner_radius={corner_radius}
-                thickness={separatorThickness}
-            />
-        {/if}
-    </div>
-
     {#if showBottom}
         <Separator
             color={color}
             class='bottom'
             length={width}
-            horizontal={true}
 			add_wings={false}
+            isHorizontal={true}
+            thickness={thickness}
 			has_thin_divider={false}
-            origin={Point.y(height)}
             corner_radius={corner_radius}
-            thickness={separatorThickness}
-        />
+            origin={Point.y(height - thickness)}/>
     {/if}
 </div>
-
-<style>
-    .box {
-        box-sizing: border-box;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .content-wrapper {
-        display: flex;
-        flex: 1;
-        position: relative;
-    }
-
-    .content {
-        flex: 1;
-    }
-
-    .top-separator {
-        width: 100%;
-    }
-
-    .bottom-separator {
-        width: 100%;
-    }
-
-    .left-separator {
-        height: 100%;
-    }
-
-    .right-separator {
-        height: 100%;
-    }
-</style>
