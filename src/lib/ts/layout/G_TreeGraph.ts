ea89f34c92962d97c1ecfe74babfd381b661780d
jonathan sand <sand@gizmolab.com>
2025-08-26 10:27:44 -0700

    simplify G_TreeGraph

--- src/lib/ts/layout/G_TreeGraph.ts
+++ src/lib/ts/layout/G_TreeGraph.ts
@@ -1,20 +1,30 @@
 import { w_depth_limit, w_graph_rect, w_show_details } from "../managers/Stores";
 import { w_ancestry_focus, w_device_isMobile } from "../managers/Stores";
-import { k, Rect} from "../common/Global_Imports";
+import { k, Rect, Ancestry, G_Widget} from "../common/Global_Imports";
 import { get } from "svelte/store";
 
 export default class G_TreeGraph {
 	rect_ofTree = Rect.zero;
+	g_focus!: G_Widget;
+	focus!: Ancestry;
+
+	constructor() {
+		w_ancestry_focus.subscribe((focus: Ancestry) => {
+			if (!!focus) {
+				this.g_focus = focus.g_widget;
+				this.focus = focus;
+			}
+		});
+	}
 
 	grand_layout_tree() {
-		const depth_limit = get(w_depth_limit);
-		const ancestry = get(w_ancestry_focus);
-		const g_widget = ancestry?.g_widget;
-		if (!!g_widget) {
-			this.layout_focus_ofTree();
-			g_widget.recursively_layout_subtree(depth_limit);
-			g_widget.recursively_layout_bidirectionals(depth_limit);
-			this.adjust_focus_ofTree();
+		const graph_rect = get(w_graph_rect);
+		const depth_limit = get(w_depth_limit) ?? 1;
+		if (!!graph_rect && !!this.g_focus) {
+			this.layout_focus_ofTree(graph_rect);
+			this.g_focus.recursively_layout_subtree(depth_limit);
+			this.g_focus.recursively_layout_bidirectionals(depth_limit);
+			this.adjust_focus_ofTree(graph_rect);
 			this.update_rect_ofTree();
 		}
 	}
@@ -22,47 +32,35 @@ export default class G_TreeGraph {
 	static readonly _____PRIVATE: unique symbol;
 
 	private update_rect_ofTree() {
-		const ancestry = get(w_ancestry_focus);
-		const g_widget = ancestry?.g_widget;
-		const size = ancestry.size_ofVisibleSubtree.expandedByX(40);
-		const origin = g_widget.origin_ofWidget.offsetByXY(3, k.height.row - size.height / 2);
+		const size = this.focus.size_ofVisibleSubtree.expandedByX(40);
+		const origin = this.g_focus.origin_ofWidget.offsetByXY(3, k.height.row - size.height / 2);
 		this.rect_ofTree = new Rect(origin, size);
 	}
 
-	private adjust_focus_ofTree() {
-		const graph_rect = get(w_graph_rect);
-		const ancestry = get(w_ancestry_focus);
-		const g_widget = ancestry?.g_widget;
-		if (!!graph_rect && !!ancestry && !!g_widget) {
-			const y_offset = -1 - graph_rect.origin.y;
-			const subtree_size = ancestry.size_ofVisibleSubtree;
-			const x_offset_ofReveal = (ancestry.thing?.width_ofTitle ?? 0) / 2 - 2;
-			const x_offset_forDetails = (get(w_show_details) ? -k.width.details : 0);
-			const x_offset = 15 + x_offset_forDetails - (subtree_size.width / 2) - (k.height.dot / 2.5) + x_offset_ofReveal;
-			const origin_ofFocusReveal = graph_rect.center.offsetByXY(x_offset, y_offset);
-			g_widget.origin_ofWidget = origin_ofFocusReveal.offsetByXY(-21.5 - x_offset_ofReveal, -5);
-		}
+	private adjust_focus_ofTree(graph_rect: Rect) {
+		const y_offset = -1 - graph_rect.origin.y;
+		const subtree_size = this.focus.size_ofVisibleSubtree;
+		const x_offset_ofReveal = (this.focus.thing?.width_ofTitle ?? 0) / 2 - 2;
+		const x_offset_forDetails = (get(w_show_details) ? -k.width.details : 0);
+		const x_offset = 15 + x_offset_forDetails - (subtree_size.width / 2) - (k.height.dot / 2.5) + x_offset_ofReveal;
+		const origin_ofFocusReveal = graph_rect.center.offsetByXY(x_offset, y_offset);
+		this.g_focus.origin_ofWidget = origin_ofFocusReveal.offsetByXY(-21.5 - x_offset_ofReveal, -5);
 	}
 
-	private layout_focus_ofTree() {
-		const ancestry = get(w_ancestry_focus);
-		const graph_rect = get(w_graph_rect);
-		const g_widget = ancestry?.g_widget;
-		if (!!graph_rect && !!g_widget) {
-			const y_offset = graph_rect.origin.y;
-			const subtree_size = ancestry.size_ofVisibleSubtree;
-			const x_offset_ofFirstReveal = (ancestry.thing?.width_ofTitle ?? 0) / 2 - 2;
-			const y_offset_ofBranches = (k.height.dot / 2) -(subtree_size.height / 2) - 4;
-			const x_offset_ofBranches = -8 - k.height.dot + x_offset_ofFirstReveal;
-			const x_offset = (get(w_show_details) ? -k.width.details : 0) + 15 + x_offset_ofFirstReveal - (subtree_size.width / 2) - (k.height.dot / 2.5);
-			const origin_ofFocusReveal = graph_rect.center.offsetByXY(x_offset, -y_offset);
-			if (get(w_device_isMobile)) {
-				origin_ofFocusReveal.x = 25;
-			}
-			// need this for laying out branches, but it is wrong for final positioning
-			// TODO: dunno why, must fix
-			g_widget.origin_ofWidget = origin_ofFocusReveal.offsetByXY(x_offset_ofBranches, y_offset_ofBranches);
+	private layout_focus_ofTree(graph_rect: Rect) {
+		const y_offset = graph_rect.origin.y;
+		const subtree_size = this.focus.size_ofVisibleSubtree;
+		const x_offset_ofFirstReveal = (this.focus.thing?.width_ofTitle ?? 0) / 2 - 2;
+		const y_offset_ofBranches = (k.height.dot / 2) -(subtree_size.height / 2) - 4;
+		const x_offset_ofBranches = -8 - k.height.dot + x_offset_ofFirstReveal;
+		const x_offset = (get(w_show_details) ? -k.width.details : 0) + 15 + x_offset_ofFirstReveal - (subtree_size.width / 2) - (k.height.dot / 2.5);
+		const origin_ofFocusReveal = graph_rect.center.offsetByXY(x_offset, -y_offset);
+		if (get(w_device_isMobile)) {
+			origin_ofFocusReveal.x = 25;
 		}
+		// need this for laying out branches, but it is wrong for final positioning
+		// TODO: dunno why, must fix
+		this.g_focus.origin_ofWidget = origin_ofFocusReveal.offsetByXY(x_offset_ofBranches, y_offset_ofBranches);
 	}
 
 }
