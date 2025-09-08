d3e669474ee6c8e0db2363e3e487d2fa13ad9749
jonathan sand <sand@gizmolab.com>
2025-09-08 10:11:58 -0700

    results browsing works

--- src/lib/svelte/main/Search_Results.svelte
+++ src/lib/svelte/main/Search_Results.svelte
@@ -2,11 +2,34 @@
 	import { w_search_text, w_results_token, w_selected_row, w_background_color } from '../../ts/managers/Stores';
 	import { colors, T_Search } from '../../ts/common/Global_Imports';
 	import { search } from '../../ts/managers/Search';
-	let results = [];
+	let element: HTMLDivElement;
+	let results: Thing[] = [];
 
 	$: {
 		const _ = $w_results_token;
 		results = search.results;
+	}
+
+	function handle_row_selected(event: MouseEvent, index: number) {
+		event.preventDefault();
+    	$w_selected_row = index;
+		element?.focus();
+	}
+
+	function handle_key_down(event: KeyboardEvent) {
+		switch (event.key.toLowerCase()) {
+			case 'arrowup':		event.preventDefault(); next_row(false); break;
+			case 'arrowdown':	event.preventDefault(); next_row(true); break;
+		}
+	}
+
+	function next_row(up: boolean) {
+		const row = $w_selected_row;
+		if (row !== null) {
+			const count = results.length;	// stupid, but it works
+			$w_selected_row = row.increment(up, count);
+			element?.focus();
+		}
 	}
 
 	function highlightMatch(title: string, searchText: string) {
@@ -26,30 +49,37 @@
 		result += title.slice(lastIndex);
 		return result;
 	}
+
 </script>
 
-{#key $w_background_color}
-	<div class='search-results'>
+<div class='search-results' 
+	on:keydown={handle_key_down}
+	bind:this={element}
+	tabindex='0'>
+	{#key `${$w_background_color}:::${$w_selected_row}:::${$w_results_token}`}
 		<ul>
 			{#each results as result, index}
-				<li 
-					on:mousedown={() => $w_selected_row = index}
-					class:selected={$w_selected_row === index}
-				>{@html highlightMatch(result.title, $w_search_text)}</li>
+				<li class:selected={$w_selected_row === index}
+					on:mousedown={(event) => handle_row_selected(event, index)}>
+					{@html highlightMatch(result.title, $w_search_text)}
+				</li>
 			{/each}
 		</ul>
-	</div>
-{/key}
+	{/key}
+</div>
 
 <style>
 	.search-results {
+		outline: none !important;
 		position: absolute;
 		padding-top: 8px;
+        z-index: 100;
 		height: 100%;
 		width: 100%;
 	}
 	ul {
 		list-style: none;
+		outline: none;
 		padding: 0;
 		margin: 0;
 	}
@@ -64,6 +94,6 @@
 	}
 	li.selected,
 	li.selected:hover {
-		background-color: lightblue;
+		background-color: pink;
 	}
 </style>
