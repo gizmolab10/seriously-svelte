ad7b81c890bb20aec7cf9b32e9ab365f2ac6acd3
jonathan sand <sand@gizmolab.com>
2025-12-09 12:51:55 -0800

    fix netlify build failure

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import sveltePreprocess from 'svelte-preprocess';
-
+import adapter from '@sveltejs/adapter-netlify';
 export default {
+  kit: {
+    adapter: adapter(),
+    // other options...
+  },
   preprocess: sveltePreprocess({
     scss: {
       prependData: ''
